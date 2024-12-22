e8b7df61bdd37f7c38a5051fb8cd6af7da53c093
fajar sidik priatna <jardik.oryza@gmail.com>
2024-12-22 14:28:14 +0700

    minor

--- src/BasecodeGenProvider.php
+++ src/BasecodeGenProvider.php
@@ -14,15 +14,16 @@ class BasecodeGenProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/Console/Commands/Stubs' => base_path('stubs/rgb_basecode_gen'),
-            ], 'rgb-stubs');
-
-            $this->commands([
-                MakeCmsControllerAndService::class,
-            ]);
+        if (!$this->app->runningInConsole()) {
+            return;
         }
+        $this->publishes([
+            __DIR__ . '/Console/Commands/Stubs' => base_path('stubs/rgb_basecode_gen'),
+        ], 'rgb-stubs');
+
+        $this->commands([
+            MakeCmsControllerAndService::class,
+        ]);
     }
 
     public function provides(): array
